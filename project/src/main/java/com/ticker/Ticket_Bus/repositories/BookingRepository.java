06ef3593c55033fc252308c131becd7a6a32a6eb
Lucas16AR <luqui0008@gmail.com>
2023-05-31 17:15:03 -0300

    #15 repository in process

--- project/src/main/java/com/ticker/Ticket_Bus/repositories/BookingRepository.java
+++ project/src/main/java/com/ticker/Ticket_Bus/repositories/BookingRepository.java
@@ -1,9 +1,8 @@
 package main.java.com.ticker.Ticket_Bus.repositories;
-#importa modelos
-#import ;
+import main.java.com.ticker.Ticket_Bus.models.Booking;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface BookingRepository extends CrudRepository<Booking, Long> {
 }
--- project/src/main/java/com/ticker/Ticket_Bus/repositories/TripRepository.java
+++ project/src/main/java/com/ticker/Ticket_Bus/repositories/TripRepository.java
@@ -1,3 +1,9 @@
-public class TripRepository {
-    
+package main.java.com.ticker.Ticket_Bus.repositories;
+#importa modelos
+#import ;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TripRepository extends CrudRepository<Trip, Long> {
 }
