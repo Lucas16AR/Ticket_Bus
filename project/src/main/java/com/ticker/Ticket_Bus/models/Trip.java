90ec0cf8b2198a719ab59db442ee50009ff283cd
lucilamicaelamartinez <lucilamicaelamartinez@gmail.com>
2023-05-31 12:42:19 -0300

    adding Vehicle Model y Trip Model

---
+++ project/src/main/java/com/ticker/Ticket_Bus/models/Trip.java
@@ -0,0 +1,117 @@
+import javax.persistence.*;
+import java.util.Date;
+import java.sql.Time;
+import java.util.List;
+
+@Entity
+@Table(name = "trips")
+public class Trip {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "date")
+    private Date date;
+
+    @ManyToOne
+    @JoinColumn(name = "origin_city_id")
+    private City originCity;
+
+    @ManyToOne
+    @JoinColumn(name = "destination_city_id")
+    private City destinationCity;
+
+    @Column(name = "trip_time")
+    private Time tripTime;
+
+    @Column(name = "distance")
+    private int distance;
+
+    @ManyToOne
+    @JoinColumn(name = "vehicle_id")
+    private Vehicle vehicle;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
+
+    @OneToMany(mappedBy = "trip")
+    private List<Booking> bookings;
+
+    public Trip() {
+    }
+
+    // Getters and Setters...
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public City getOriginCity() {
+        return originCity;
+    }
+
+    public void setOriginCity(City originCity) {
+        this.originCity = originCity;
+    }
+
+    public City getDestinationCity() {
+        return destinationCity;
+    }
+
+    public void setDestinationCity(City destinationCity) {
+        this.destinationCity = destinationCity;
+    }
+
+    public Time getTripTime() {
+        return tripTime;
+    }
+
+    public void setTripTime(Time tripTime) {
+        this.tripTime = tripTime;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+}
---
+++ project/src/main/java/com/ticker/Ticket_Bus/models/Vehicle.java
@@ -0,0 +1,79 @@
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "vehicles")
+public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "seats")
+    private int seats;
+
+    @Column(name = "type")
+    private String type;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
+
+    @OneToMany(mappedBy = "vehicle")
+    private List<Trip> trips;
+
+    public Vehicle() {
+    }
+
+    // Getters and Setters...
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public List<Trip> getTrips() {
+        return trips;
+    }
+
+    public void setTrips(List<Trip> trips) {
+        this.trips = trips;
+    }
+}
